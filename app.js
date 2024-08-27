aac5d430d834c2a92eb59aaa6f3391454ee81817
harbiordun85 <adelinux@yahoo.com>
2024-08-27 13:33:12 -0400

    2024 commit review2 stage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Abiodun_Adejumo CI/CD CloudProject_All of project Expr'));
+app.get('/', (req, res) => res.send('Welcome to Abiodun_Adejumo_Sample CI/CD CloudProject with Code change push succeded, with All of project Exprience'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
