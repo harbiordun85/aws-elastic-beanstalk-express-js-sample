4b5ead116b70d9d9b021f4cc751fe182d8a28c69
harbiordun85 <adelinux@yahoo.com>
2024-08-27 13:11:16 -0400

    2024 commit review stage

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Abiodun_Adejumo CloudProject'));
+app.get('/', (req, res) => res.send('Welcome to Abiodun_Adejumo CloudProject_I have experience configuring systems 
+    in AWS, Azure, assisting businesses with the adoption of hybrid infrastructure, and with minimizing fixed costs associated 
+    with 100% on-premise infrastructure. This includes tasks VPC Networking and All type  cloud Resources, configuring
+    web servers, and other related activities.'));
+    
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
